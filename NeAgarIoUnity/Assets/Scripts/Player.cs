51721bec4560947190b82978cf6d8b63001bd789
Alex Zmiev <alex.zmiev.az@gmail.com>
2021-04-19 17:31:29 +0300

    что-то сломалось пока код не открывался, пофиксил

--- NeAgarIoUnity/Assets/Scripts/Player.cs
+++ NeAgarIoUnity/Assets/Scripts/Player.cs
@@ -23,10 +23,19 @@ public class Player : NetworkBehaviour
 
     [SyncVar(hook = nameof(OnNameChanged))]
     private string playerName;
+    [SyncVar(hook = nameof(OnCompStateChanged))]
+    private bool compState = true;
 
-    void OnNameChanged(string _Old, string _New)
+    private void OnNameChanged(string _Old, string _New)
     {
         playerNameText.text = playerName;
+    }
+
+    private void OnCompStateChanged(bool _Old, bool _New)
+    {
+        this.GetComponent<Collider2D>().enabled = compState;
+        this.GetComponent<SpriteRenderer>().enabled = compState;
+        playerNameText.GetComponent<MeshRenderer>().enabled = compState;
     }
 
     [Command]
@@ -36,13 +45,20 @@ public class Player : NetworkBehaviour
     }
 
     [Command]
-    public void ChangePlayerPosition()
+    public void CmdChangeCompState(bool b)
     {
-        this.transform.position = new Vector3(
-            Random.Range(-GameGlobalSettings.GameField.x, GameGlobalSettings.GameField.x),
-            Random.Range(-GameGlobalSettings.GameField.y, GameGlobalSettings.GameField.y),
-            0);
+        compState = b;
     }
+
+    //[Command]
+    //public void CmdChangePlayerPosition()
+    //{
+    //    this.transform.position = new Vector3(
+    //        Random.Range(-GameGlobalSettings.GameField.x, GameGlobalSettings.GameField.x),
+    //        Random.Range(-GameGlobalSettings.GameField.y, GameGlobalSettings.GameField.y),
+    //        0);
+    //}
+
 
     public override void OnStartAuthority()
     {
@@ -68,30 +84,31 @@ public class Player : NetworkBehaviour
         CmdSetupPlayer(playerName);
         playerNameText.text = playerName;
 
-        ChangePlayerPosition();
+        //CmdChangePlayerPosition();
     }
+
 
     private void FixedUpdate()
     {
         if (!hasAuthority) { return; }
         if (isAlive)
         {
-            Vector2 cursorPos = PlayerCamera.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 vectorDelta = cursorPos - new Vector2(
-                this.gameObject.transform.position.x,
-                this.gameObject.transform.position.y);
+            //Vector2 cursorPos = PlayerCamera.ScreenToWorldPoint(Input.mousePosition);
+            //Vector2 vectorDelta = cursorPos - new Vector2(
+            //    this.gameObject.transform.position.x,
+            //    this.gameObject.transform.position.y);
 
-            PlayerRigidbody2D.velocity = new Vector2(
-                Mathf.Clamp(vectorDelta.x, -Speed, Speed),
-                Mathf.Clamp(vectorDelta.y, -Speed, Speed)).normalized * Speed;
+            //PlayerRigidbody2D.velocity = new Vector2(
+            //    Mathf.Clamp(vectorDelta.x, -Speed, Speed),
+            //    Mathf.Clamp(vectorDelta.y, -Speed, Speed)).normalized * Speed;
 
             this.transform.position = new Vector3(
                 Mathf.Clamp(this.transform.position.x, -AllowedRadius.x, AllowedRadius.x),
                 Mathf.Clamp(this.transform.position.y, -AllowedRadius.y, AllowedRadius.y),
                 0);
 
-            //Vector2 v2 = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            //PlayerRigidbody2D.velocity = v2.normalized * Speed;
+            Vector2 v2 = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            PlayerRigidbody2D.velocity = v2.normalized * Speed;
         }
     }
 
@@ -104,9 +121,10 @@ public class Player : NetworkBehaviour
                 Weight.AddWeight(_w.GetWeight() / 10);
                 StopAllCoroutines();
                 StartCoroutine(ChangeScale());
+
                 if (collision.TryGetComponent(out Player _p))
                 {
-                    _p.Deth();
+                    _p.Death();
                 }
                 else
                 {
@@ -141,15 +159,16 @@ public class Player : NetworkBehaviour
         }
     }
 
-    public void Deth()
+
+    public void Death()
     {
         isAlive = false;
-        PlayerRigidbody2D.velocity = Vector3.zero;
 
-        this.GetComponent<Collider2D>().enabled = false;
-        this.GetComponent<SpriteRenderer>().enabled = false;
-        playerNameText.GetComponent<MeshRenderer>().enabled = false;
-        DeathCanvas.SetActive(true);
+        PlayerRigidbody2D.velocity = Vector3.zero;
+        CmdChangeCompState(false);
+
+        if (hasAuthority)
+            DeathCanvas.SetActive(true);
 
         ExternalListener.SendResults(PlayerId + " " + this.GetComponent<Weight>().GetWeight());
     }
@@ -158,11 +177,9 @@ public class Player : NetworkBehaviour
     {
         isAlive = true;
 
-        ChangePlayerPosition();
-
-        this.GetComponent<Collider2D>().enabled = true;
-        this.GetComponent<SpriteRenderer>().enabled = true;
-        playerNameText.GetComponent<MeshRenderer>().enabled = true;
+        //CmdChangePlayerPosition();
+        CmdChangeCompState(true);
+        
         DeathCanvas.SetActive(false);
 
         Weight.ResetWeight();
